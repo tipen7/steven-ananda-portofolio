244fee1bfa7bfeeea09119d36e9e88191c424841
tipen7 <tipdyanizha@gmail.com>
2025-08-03 17:46:00 +0700

    Updated Loading Screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
     const [text, setText] = useState("");
-    const fullText = "MY PORTFOLIO WEBSITE";
+    const fullText = "TUGAS KHUSUS";
 
     useEffect (() => {
         let index = 0;
