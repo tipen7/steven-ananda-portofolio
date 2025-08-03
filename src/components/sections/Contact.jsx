d30248a127f8eb0f76954f9d51c58edc641de576
tipen7 <tipdyanizha@gmail.com>
2025-08-03 17:44:12 +0700

    Updated Contacts

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -31,7 +31,7 @@ export const Contact = () => {
         className="min-h-screen flex items-center justify-center py-20">
 
             <Animation>
-            <div className="px-4 w-150">
+            <div className="px-4 w-full max-w-xl mx-auto">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                     Contact Me!
                 </h2>
