88209773241b707514ff07498e39ce4a7c63dea8
tipen7 <tipdyanizha@gmail.com>
2025-08-03 17:34:49 +0700

    Updated Mobile Menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -3,9 +3,9 @@ import { useEffect } from "react";
 export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
   return (
     <div
-      className={`fixed top-0 left-0 z-60 w-full bg-[rgba(10,10,10,0.8)] flex flex-col items-center justify-center 
+      className={`fixed top-0 left-0 z-50 w-full bg-[rgba(10,10,10,0.8)] flex flex-col items-center justify-center 
         transition-all duration-300 ease-in-out 
-        ${menuOpen ? "h-screen opacity-100 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}
+        ${menuOpen ? "min-h-screen opacity-100 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}
     >
       <button
         onClick={() => setMenuOpen(false)}
